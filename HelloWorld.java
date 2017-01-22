165876dd862d8fc936996f0bad6b5b14543477e4
wangjinlun <wangjinlun@139.com>
2017-01-22 09:47:22 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,16 @@ public class HelloWorld{
     catch (Exception e) {  // 不规范
       throw e;
     }
+    
+    int param =0 ;
+    param =4;
+    switch (param) {  //没有default语句
+    case 0:
+      doSomething();
+      break;
+    case 1:
+      doSomethingElse();
+      break;
+    }
   }
 }

7ccf347c79fcd5b151b0c67c7882580aa4e7ad9f
wangjinlun <wangjinlun@139.com>
2017-01-22 09:41:10 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hello World!");
+    string s = "";
+    try {
+      s = "Hello world";
+    }
+    catch (Exception e) {  // 不规范
+      throw e;
+    }
   }
 }
